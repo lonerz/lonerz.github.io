a626a8806036affc71465db9df1a46cb38ab7f81
Joshua Pan <joshuapan8@gmail.com>
2017-10-22 15:51:58 -0700

    Add coloring game.

---
+++ js/canvas.js
@@ -0,0 +1,28 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+function circle(ctx, cx, cy, r, fillStyle, lineWidth, strokeStyle) {
+    ctx.lineWidth = lineWidth;
+    ctx.fillStyle = fillStyle;
+    ctx.strokeStyle = strokeStyle || fillStyle;
+    ctx.beginPath();
+    ctx.arc(cx, cy, r, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function drawEdge(ctx, x1, y1, x2, y2) {
+    ctx.lineWidth = 15;
+    ctx.strokeStyle = "#D0D0D0";
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+function inCircle(x, y, cx, cy, r) {
+    return (x - cx)*(x - cx) + (y - cy)*(y - cy) <= r*r;
+}
+
